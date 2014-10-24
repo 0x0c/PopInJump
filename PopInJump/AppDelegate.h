b5e974914e4064310dfc686ee24c4279b44a9d9c
Akira Matsuda <akira.m.itachi@gmail.com>
2014-10-25 06:17:29 +0900

    Initial Commit

---
+++ PopInJump/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PopInJump
+//
+//  Created by Akira Matsuda on 10/25/14.
+//  Copyright (c) 2014 Akira Matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PopInJump/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PopInJump
+//
+//  Created by Akira Matsuda on 10/25/14.
+//  Copyright (c) 2014 Akira Matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
